2fcabe7582130154f6e1ed938dfa34bca015159a
JaranOlsen <jaran.olsen@icloud.com>
2022-08-31 23:35:01 +0200

    “deploy”

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,37 @@
 #!/usr/bin/env sh
 
 # abort on errors
-set -e
+#set -e
 
 # build
-npm run build
+#npm run build
 
 # navigate into the build output directory
-cd dist
+#cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
+#git init
+#git checkout -b main
+#git add -A
+#git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:JaranOlsen/Planet.git main:gh-pages
+#git push -f git@github.com:JaranOlsen/Planet.git main:gh-pages
 
-cd -
+#cd -
+
+git init
+git add .
+git commit -m “deploy”
+git branch -M main
+git push -u origin main
+
+npm run build
+git add dist -f
+git commit -m “something”
+git subtree push --prefix dist origin gh-pages
