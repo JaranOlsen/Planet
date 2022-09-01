c802662346ff658e4ae39e87cf85c4712cfbe16f
JaranOlsen <jaran.olsen@icloud.com>
2022-09-01 21:26:46 +0200

    pleeeeaaaase

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vitePluginString from 'vite-plugin-string'
-
-export default defineConfig({
-  base: '/Planet/',
-  plugins: [
-    vitePluginString.default(),
-  ]
-})
