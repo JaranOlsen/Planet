42601609a2409c2f2344bb45b0ec41fbdd0d93d7
JaranOlsen <jaran.olsen@icloud.com>
2022-09-23 23:55:42 +0200

    vr test

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
 import * as THREE from 'three';
-//import * as YUKA from 'yuka';
-import {Float32BufferAttribute, FrontSide, AdditiveBlending, BackSide, DoubleSide, Vector3, RGBADepthPacking, SubtractiveBlending} from 'three';
+import { Float32BufferAttribute, FrontSide, AdditiveBlending, BackSide, DoubleSide, Vector3, RGBADepthPacking, SubtractiveBlending } from 'three';
 import { tagList } from './tagData.js'
 import { imgList } from './imgData.js'
 import { tagConnections } from './tagConnectionData.js'
 import { palette } from './palette.js'
-import {getRandomNum, getRandomBell, getRandomInt, convertLatLngtoCartesian, convertCartesiantoLatLng} from './mathScripts.js'
-import {OrbitControls} from "three/addons/controls/OrbitControls.js";
+import { getRandomNum, getRandomBell, getRandomInt, convertLatLngtoCartesian, convertCartesiantoLatLng } from './mathScripts.js'
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { Lensflare, LensflareElement } from 'three/addons/objects/Lensflare.js'; 
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 import { XRControllerModelFactory } from 'three/examples/jsm/webxr/XRControllerModelFactory.js';
 import { GUI } from 'dat.gui'
-import {
-	Constants as MotionControllerConstants,
-	fetchProfile,
-	MotionController
-} from 'three/examples/jsm/libs/motion-controllers.module.js';
-//import { CanvasUI } from './CanvasUI.js';
-import Stats from 'three/examples/jsm/libs/stats.module'; //FIX?
 
 //    IMPORT SHADERS
 import vertexShader from './shaders/vertex.glsl';
@@ -52,6 +44,7 @@ const renderer = new THREE.WebGLRenderer(
         antialias: true,
     });
 renderer.setPixelRatio(window.devicePixelRatio)
+renderer.outputEncoding = THREE.sRGBEncoding;
 
 function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -63,8 +56,6 @@ function resizeRendererToDisplaySize(renderer) {
     }
     return needResize;
 }
-
-let clock = new THREE.Clock();
 
 const fov = 50;
 const aspect = 2;  // the canvas default
@@ -90,335 +81,146 @@ let selectedPin = null;
 
 const middleOfPlanet = new THREE.Vector3(0, 0, 0);
 
-//enable VR
+//VR
+renderer.shadowMap.enabled = true; //ANY SHADOWS? WORTH PROCESSING COST?
+renderer.xr.enabled = true;
+let controller1, controller2;
+let controllerGrip1, controllerGrip2;
+const intersected = [];
+const tempMatrix = new THREE.Matrix4();
+let group;
+group = new THREE.Group();
+scene.add( group );
+document.body.appendChild( VRButton.createButton( renderer ) );
 
-const DEFAULT_PROFILES_PATH = 'https://cdn.jsdelivr.net/npm/@webxr-input-profiles/assets@1.0/dist/profiles';
-const DEFAULT_PROFILE = 'generic-trigger';
-const stats = new Stats();
-document.body.appendChild( stats.dom );
-let workingMatrix = new THREE.Matrix4();
-let origin = new THREE.Vector3();
-//let ui = createUI();
-let controllers = {};
+this.dolly = new THREE.Object3D();
+this.dolly.position.z = 8;
+this.dolly.add( this.camera );
+this.scene.add( this.dolly );
 
+this.dummyCam = new THREE.Object3D();
+this.camera.add( this.dummyCam );
 
-setupXR();
-/* function createUI(){
-    const config = {
-        panelSize: { height: 0.5 },
-        height: 256,
-        body: { type: "text" }
+// controllers
+
+controller1 = renderer.xr.getController( 0 );
+controller1.addEventListener( 'selectstart', onSelectStart );
+controller1.addEventListener( 'selectend', onSelectEnd );
+scene.add( controller1 );
+
+controller2 = renderer.xr.getController( 1 );
+controller2.addEventListener( 'selectstart', onSelectStart );
+controller2.addEventListener( 'selectend', onSelectEnd );
+scene.add( controller2 );
+
+const controllerModelFactory = new XRControllerModelFactory();
+
+controllerGrip1 = renderer.xr.getControllerGrip( 0 );
+controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
+scene.add( controllerGrip1 );
+
+controllerGrip2 = renderer.xr.getControllerGrip( 1 );
+controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
+scene.add( controllerGrip2 );
+
+//
+
+const geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 1 ) ] );
+
+const line = new THREE.Line( geometry );
+line.name = 'line';
+line.scale.z = 5;
+
+controller1.add( line.clone() );
+controller2.add( line.clone() );
+
+//
+
+function onSelectStart( event ) {
+
+    const controller = event.target;
+
+    const intersections = getIntersections( controller );
+
+    if ( intersections.length > 0 ) {
+
+        const intersection = intersections[ 0 ];
+
+        const object = intersection.object;
+        object.material.emissive.b = 1;
+        controller.attach( object );
+
+        controller.userData.selected = object;
+
     }
-    const ui = new CanvasUI( { body: "" }, config );
-    ui.mesh.position.set(0, 1.5, -1);
-    scene.add( ui.mesh );
-    return ui;
-} */
-function createButtonStates(components){
-
-    let buttonStates = {};
-    let gamepadIndices = components;
-    
-    Object.keys( components ).forEach( (key) => {
-        if ( key.indexOf('touchpad')!=-1 || key.indexOf('thumbstick')!=-1){
-            buttonStates[key] = { button: 0, xAxis: 0, yAxis: 0 };
-        }else{
-            buttonStates[key] = 0; 
-        }
-    })
-    
-    //buttonStates = buttonStates;
-}
-
-/* function updateUI(){
-    const str = JSON.stringify( buttonStates );
-    if (strStates === undefined || ( str != strStates )){
-        ui.updateElement( 'body', str );
-        ui.update(); 
-        strStates = str;
-    }
-} */
-
-/* function updateGamepadState(){
-    const session = renderer.xr.getSession();
-    
-    const inputSource = session.inputSources[0];
-    
-    if (inputSource && inputSource.gamepad && gamepadIndices && ui && buttonStates){
-        const gamepad = inputSource.gamepad;
-        try{
-            Object.entries( buttonStates ).forEach( ( [ key, value ] ) => {
-                const buttonIndex = gamepadIndices[key].button;
-                if ( key.indexOf('touchpad')!=-1 || key.indexOf('thumbstick')!=-1){
-                    const xAxisIndex = gamepadIndices[key].xAxis;
-                    const yAxisIndex = gamepadIndices[key].yAxis;
-                    buttonStates[key].button = gamepad.buttons[buttonIndex].value; 
-                    buttonStates[key].xAxis = gamepad.axes[xAxisIndex].toFixed(2); 
-                    buttonStates[key].yAxis = gamepad.axes[yAxisIndex].toFixed(2); 
-                }else{
-                    buttonStates[key] = gamepad.buttons[buttonIndex].value;
-                }
-                
-                updateUI();
-            });
-        }catch(e){
-            console.warn("An error occurred setting the ui");
-        }
-    }
-} */
-
-function setupXR(){
-    renderer.xr.enabled = true;
-    
-    const button = new VRButton( renderer );
-
-    document.body.appendChild( VRButton.createButton( renderer ) );
-
-    const self = this;
-    
-    function onConnected( event ){
-        const info = {};
-        
-        fetchProfile( event.data, DEFAULT_PROFILES_PATH, DEFAULT_PROFILE ).then( ( { profile, assetPath } ) => {
-            console.log( JSON.stringify(profile));
-            
-            info.name = profile.profileId;
-            info.targetRayMode = event.data.targetRayMode;
-
-            Object.entries( profile.layouts ).forEach( ( [key, layout] ) => {
-                const components = {};
-                Object.values( layout.components ).forEach( ( component ) => {
-                    components[component.rootNodeName] = component.gamepadIndices;
-                });
-                info[key] = components;
-            });
-
-            createButtonStates( info.right );
-            
-            console.log( JSON.stringify(info) );
-
-            updateControllers( info );
-
-        } );
-    }
-     
-    const controller = renderer.xr.getController( 0 );
-    
-    controller.addEventListener( 'connected', onConnected );
-    
-    const modelFactory = new XRControllerModelFactory();
-    
-    const geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3( 0,0,0 ), new THREE.Vector3( 0,0,-1 ) ] );
-
-    const line = new THREE.Line( geometry );
-    line.scale.z = 0;
-    
-    //let controllers = {};
-    controllers.right = buildController( 0, line, modelFactory );
-    controllers.left = buildController( 1, line, modelFactory );
-
-    let dolly = new THREE.Object3D();
-    dolly.position.z = 6;
-    dolly.add( camera );
-    scene.add( dolly );
-    
-    let dummyCam = new THREE.Object3D();
-    camera.add( dummyCam );
 
 }
 
-function buildController( index, line, modelFactory ){
-    const controller = renderer.xr.getController( index );
-    
-    controller.userData.selectPressed = false;
-    controller.userData.index = index;
-    
-    if (line) controller.add( line.clone() );
-    
-    scene.add( controller );
-    
-    let grip;
-    
-    if ( modelFactory ){
-        grip = renderer.xr.getControllerGrip( index );
-        grip.add( modelFactory.createControllerModel( grip ));
-        scene.add( grip );
+function onSelectEnd( event ) {
+
+    const controller = event.target;
+
+    if ( controller.userData.selected !== undefined ) {
+
+        const object = controller.userData.selected;
+        object.material.emissive.b = 0;
+        group.attach( object );
+
+        controller.userData.selected = undefined;
+
     }
-    
-    return { controller, grip };
+
+
 }
 
-function updateControllers(info){
-    const self = this;
-    
-    function onSelectStart( ){
-        userData.selectPressed = true;
-    }
+function getIntersections( controller ) {
 
-    function onSelectEnd( ){
-        children[0].scale.z = 0;
-        userData.selectPressed = false;
-        userData.selected = undefined;
-    }
+    tempMatrix.identity().extractRotation( controller.matrixWorld );
 
-    function onSqueezeStart( ){
-        userData.squeezePressed = true;
-        if (userData.selected !== undefined ){
-            attach( userData.selected );
-            userData.attachedObject = userData.selected;
-        }
-    }
+    raycaster.ray.origin.setFromMatrixPosition( controller.matrixWorld );
+    raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
 
-    function onSqueezeEnd( ){
-        userData.squeezePressed = false;
-        if (userData.attachedObject !== undefined){
-            self.room.attach( userData.attachedObject );
-            userData.attachedObject = undefined;
-        }
-    }
-    
-    function onDisconnected(){
-        const index = userData.index;
-        console.log(`Disconnected controller ${index}`);
-        
-        if ( self.controllers ){
-            const obj = (index==0) ? self.controllers.right : self.controllers.left;
-            
-            if (obj){
-                if (obj.controller){
-                    const controller = obj.controller;
-                    while( controller.children.length > 0 ) controller.remove( controller.children[0] );
-                    self.scene.remove( controller );
-                }
-                if (obj.grip) self.scene.remove( obj.grip );
-            }
-        }
-    }
-    
-    if (info.right !== undefined){
-        const right = renderer.xr.getController(0);
-        
-        let trigger = false, squeeze = false;
-        
-        Object.keys( info.right ).forEach( (key) => {
-            if (key.indexOf('trigger')!=-1) trigger = true;                   if (key.indexOf('squeeze')!=-1) squeeze = true;      
-        });
-        
-        if (trigger){
-            right.addEventListener( 'selectstart', onSelectStart );
-            right.addEventListener( 'selectend', onSelectEnd );
-        }
+    return raycaster.intersectObjects( group.children, false );
 
-        if (squeeze){
-            right.addEventListener( 'squeezestart', onSqueezeStart );
-            right.addEventListener( 'squeezeend', onSqueezeEnd );
-        }
-        
-        right.addEventListener( 'disconnected', onDisconnected );
-    }
-    
-    if (info.left !== undefined){
-        const left = renderer.xr.getController(1);
-        
-        let trigger = false, squeeze = false;
-        
-        Object.keys( info.left ).forEach( (key) => {
-            if (key.indexOf('trigger')!=-1) trigger = true;                   if (key.indexOf('squeeze')!=-1) squeeze = true;      
-        });
-        
-        if (trigger){
-            left.addEventListener( 'selectstart', onSelectStart );
-            left.addEventListener( 'selectend', onSelectEnd );
-        }
-
-        if (squeeze){
-            left.addEventListener( 'squeezestart', onSqueezeStart );
-            left.addEventListener( 'squeezeend', onSqueezeEnd );
-        }
-        
-        left.addEventListener( 'disconnected', onDisconnected );
-    }
 }
 
-function handleController( controller ){
-/*     if (controller.userData.selectPressed ){
-        controller.children[0].scale.z = 10;
+function intersectObjects( controller ) {
 
-        workingMatrix.identity().extractRotation( controller.matrixWorld );
+    // Do not highlight when already selected
 
-        raycaster.ray.origin.setFromMatrixPosition( controller.matrixWorld );
-        raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( workingMatrix );
+    if ( controller.userData.selected !== undefined ) return;
 
-        const intersects = raycaster.intersectObjects( pinPositions );
+    const line = controller.getObjectByName( 'line' );
+    const intersections = getIntersections( controller );
 
-        if (intersects.length>0){
-            intersects[0].object.add(highlight);
-            highlight.visible = true;
-            controller.children[0].scale.z = intersects[0].distance;
-            controller.userData.selected = intersects[0].object;
-        }else{
-            highlight.visible = false;
-        }
-    } */
-    if (controller.userData.selectPressed ){
-            
-        const wallLimit = 0.5;
-        const speed = 0.2;
-        let pos = dolly.position.clone();
-        pos.y += 1;
+    if ( intersections.length > 0 ) {
 
-        let dir = new THREE.Vector3();
-        //Store original dolly rotation
-        const quaternion = dolly.quaternion.clone();
-        //Get rotation for movement from the headset pose
-        dolly.quaternion.copy( dummyCam.getWorldQuaternion() );
-        dolly.getWorldDirection(dir);
-        dir.negate();
-        raycaster.set(pos, dir);
+        const intersection = intersections[ 0 ];
 
-        let blocked = false;
+        const object = intersection.object;
+        object.material.emissive.r = 1;
+        intersected.push( object );
 
-        let intersect = raycaster.intersectObjects(jaranius);
-        if (intersect.length>0){
-            if (intersect[0].distance < wallLimit) blocked = true;
-        }
+        line.scale.z = intersection.distance;
 
-        if (!blocked){
-            dolly.translateZ(-dt*speed);
-            pos = dolly.getWorldPosition( origin );
-        }
+    } else {
 
-        //cast left
-        dir.set(-1,0,0);
-        dir.applyMatrix4(dolly.matrix);
-        dir.normalize();
-        raycaster.set(pos, dir);
-
-        intersect = raycaster.intersectObjects(jaranius);
-        if (intersect.length>0){
-            if (intersect[0].distance<wallLimit) dolly.translateX(wallLimit-intersect[0].distance);
-        }
-
-        //cast right
-        dir.set(1,0,0);
-        dir.applyMatrix4(dolly.matrix);
-        dir.normalize();
-        raycaster.set(pos, dir);
-
-        intersect = raycaster.intersectObjects(jaranius);
-        if (intersect.length>0){
-            if (intersect[0].distance<wallLimit) dolly.translateX(intersect[0].distance-wallLimit);
-        }
-
-        dolly.position.y = 0;
-
-        //Restore the original rotation
-        dolly.quaternion.copy( quaternion );
+        line.scale.z = 5;
 
     }
+
 }
 
+function cleanIntersected() {
 
+    while ( intersected.length ) {
+
+        const object = intersected.pop();
+        object.material.emissive.r = 0;
+
+    }
+
+}
 
 
 //start intro
@@ -539,7 +341,7 @@ const jaranius = new THREE.Mesh(
         side: FrontSide,
     })
 )
-scene.add(jaranius)
+group.add(jaranius)
 
 // create cloud layer
 const clouds1 = new THREE.Mesh(
@@ -1222,22 +1024,12 @@ function render(time) {
     time *= 0.001;
 
     //VR
-    if (renderer.xr.isPresenting){
-        if (controllers ){
-            Object.values( controllers).forEach( ( value ) => {
-                handleController( value.controller );
-            });
-        } 
-        /* if (elapsedTime===undefined) elapsedTime = 0;
-        elapsedTime += dt;
-        if (elapsedTime > 0.3){
-            updateGamepadState();
-            elapsedTime = 0; */
-        }
-    //}else{
-        //stats.update();
-    //}
+    cleanIntersected();
 
+				intersectObjects( controller1 );
+				intersectObjects( controller2 );
+
+    //
 
     if (resizeRendererToDisplaySize(renderer)) {
         const canvas = renderer.domElement;
@@ -1292,9 +1084,7 @@ function render(time) {
     clouds1.rotation.y += 0.00001;
     clouds2.rotation.y += 0.00005;
     clouds3.rotation.y += 0.0001;
-    //VR TEST
-    jaranius.rotation.y += 0.0005;
-    //VR TEST
+
     if (camera.position.z > 15 && start == true) {
         camera.position.z -= 0.0001 * Math.pow(camera.position.z - 10, 1.35)
         jaranius.rotation.y += 0.0005;
