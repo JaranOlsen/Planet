07464f58cfd9ca1d58e7fa0eb8feb82f3ab7e847
JaranOlsen <jaran.olsen@icloud.com>
2022-09-23 22:32:49 +0200

    vr test

--- main.js
+++ main.js
@@ -217,6 +217,14 @@ function setupXR(){
     controllers.right = buildController( 0, line, modelFactory );
     controllers.left = buildController( 1, line, modelFactory );
 
+    let dolly = new THREE.Object3D();
+    dolly.position.z = 6;
+    dolly.add( camera );
+    scene.add( dolly );
+    
+    let dummyCam = new THREE.Object3D();
+    camera.add( dummyCam );
+
 }
 
 function buildController( index, line, modelFactory ){
@@ -333,7 +341,7 @@ function updateControllers(info){
 }
 
 function handleController( controller ){
-    if (controller.userData.selectPressed ){
+/*     if (controller.userData.selectPressed ){
         controller.children[0].scale.z = 10;
 
         workingMatrix.identity().extractRotation( controller.matrixWorld );
@@ -351,8 +359,65 @@ function handleController( controller ){
         }else{
             highlight.visible = false;
         }
+    } */
+    if (controller.userData.selectPressed ){
+            
+        const wallLimit = 0.5;
+        const speed = 0.2;
+        let pos = dolly.position.clone();
+        pos.y += 1;
+
+        let dir = new THREE.Vector3();
+        //Store original dolly rotation
+        const quaternion = dolly.quaternion.clone();
+        //Get rotation for movement from the headset pose
+        dolly.quaternion.copy( dummyCam.getWorldQuaternion() );
+        dolly.getWorldDirection(dir);
+        dir.negate();
+        raycaster.set(pos, dir);
+
+        let blocked = false;
+
+        let intersect = raycaster.intersectObjects(jaranius);
+        if (intersect.length>0){
+            if (intersect[0].distance < wallLimit) blocked = true;
+        }
+
+        if (!blocked){
+            dolly.translateZ(-dt*speed);
+            pos = dolly.getWorldPosition( origin );
+        }
+
+        //cast left
+        dir.set(-1,0,0);
+        dir.applyMatrix4(dolly.matrix);
+        dir.normalize();
+        raycaster.set(pos, dir);
+
+        intersect = raycaster.intersectObjects(jaranius);
+        if (intersect.length>0){
+            if (intersect[0].distance<wallLimit) dolly.translateX(wallLimit-intersect[0].distance);
+        }
+
+        //cast right
+        dir.set(1,0,0);
+        dir.applyMatrix4(dolly.matrix);
+        dir.normalize();
+        raycaster.set(pos, dir);
+
+        intersect = raycaster.intersectObjects(jaranius);
+        if (intersect.length>0){
+            if (intersect[0].distance<wallLimit) dolly.translateX(intersect[0].distance-wallLimit);
+        }
+
+        dolly.position.y = 0;
+
+        //Restore the original rotation
+        dolly.quaternion.copy( quaternion );
+
     }
 }
+
 
 
 
