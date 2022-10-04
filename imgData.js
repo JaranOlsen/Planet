3d2d4911dbb73443d6c4743088d5bec82cd753eb
JaranOlsen <jaran.olsen@icloud.com>
2022-10-05 00:27:28 +0200

    dev improvements

--- imgData.js
+++ imgData.js
@@ -12,18 +12,18 @@ import yellow from "./img/features/yellow.webp"
 import turquoise from "./img/features/turquoise.webp"
 import coral from "./img/features/coral.webp"
 
-export const imgList = [ //ID, SRC, LATITUDE, LONGITUDE, SIZE, RADIUS
-["CD8700A2-C44A-4F60-8929-49109F06A53D", idealism, 23, 184, 30, 5.06, "jaranius"],
-["5FA198AA-C7EF-489E-886D-62A95A32FD09", panpsychism, 22.9, 174, 30, 5.06, "jaranius"],
-["F50C2354-B76D-461E-93C1-EEAEED59607B", physicalism, 22.9, 164, 30, 5.06, "jaranius"],
-["28581B87-4FA8-4823-AEC0-3F61955286CC", dualism, 35, 177, 30, 5.06, "jaranius"],
-["B72D4692-F427-44A0-936B-32CE2AD4C732", beige, 170, 180, 500, 7, "spiral"],
-["09326608-1F6D-47A0-B03D-79B498D418C9", purple, 145, 180, 500, 7, "spiral"],
-["BDA40792-4AF9-44D1-8A77-5EA073DDE3AC", red, 120, 180, 500, 7, "spiral"],
-["57D2F42B-8FEE-4698-B92F-5E4084A0D701", blue, 103, 180, 500, 7, "spiral"],
-["6285445D-94AE-4E33-8124-35D084681168", orange, 90, 180, 500, 7, "spiral"],
-["505245DB-D1A5-449C-A1E5-70FB78803059", green, 77, 180, 500, 7, "spiral"],
-["93A616BA-F467-44CC-A647-E16A59520701", yellow, 60, 180, 500, 7, "spiral"],
-["539BFB9A-48A3-4331-A0D5-7AD3D4A3B2B4", turquoise, 35, 180, 500, 7, "spiral"],
-["D81A60FF-C600-498C-AE8E-51FA3CEC3915", coral, 10, 180, 500, 7, "spiral"]
+export const imgList = [ //ID, SRC, LATITUDE, LONGITUDE, SIZE, RADIUS, CONTEXT
+{id: "CD8700A2-C44A-4F60-8929-49109F06A53D", src: idealism, lat: 23, lng: 184, size: 30, radius: 5.06, context: "jaranius"},
+{id: "5FA198AA-C7EF-489E-886D-62A95A32FD09", src: panpsychism, lat: 22.9, lng: 174, size: 30, radius: 5.06, context: "jaranius"},
+{id: "F50C2354-B76D-461E-93C1-EEAEED59607B", src: physicalism, lat: 22.9, lng: 164, size: 30, radius: 5.06, context: "jaranius"},
+{id: "28581B87-4FA8-4823-AEC0-3F61955286CC", src: dualism, lat: 35, lng: 177, size: 30, radius: 5.06, context: "jaranius"},
+{id: "B72D4692-F427-44A0-936B-32CE2AD4C732", src: beige, lat: 170, lng: 180, size: 500, radius: 7, context: "spiral"},
+{id: "09326608-1F6D-47A0-B03D-79B498D418C9", src: purple, lat: 145, lng: 180, size: 500, radius: 7, context: "spiral"},
+{id: "BDA40792-4AF9-44D1-8A77-5EA073DDE3AC", src: red, lat: 120, lng: 180, size: 500, radius: 7, context: "spiral"},
+{id: "57D2F42B-8FEE-4698-B92F-5E4084A0D701", src: blue, lat: 103, lng: 180, size: 500, radius: 7, context: "spiral"},
+{id: "6285445D-94AE-4E33-8124-35D084681168", src: orange, lat: 90, lng: 180, size: 500, radius: 7, context: "spiral"},
+{id: "505245DB-D1A5-449C-A1E5-70FB78803059", src: green, lat: 77, lng: 180, size: 500, radius: 7, context: "spiral"},
+{id: "93A616BA-F467-44CC-A647-E16A59520701", src: yellow, lat: 60, lng: 180, size: 500, radius: 7, context: "spiral"},
+{id: "539BFB9A-48A3-4331-A0D5-7AD3D4A3B2B4", src: turquoise, lat: 35, lng: 180, size: 500, radius: 7, context: "spiral"},
+{id: "D81A60FF-C600-498C-AE8E-51FA3CEC3915", src: coral, lat: 10, lng: 180, size: 500, radius: 7, context: "spiral"},
 ]
