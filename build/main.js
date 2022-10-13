107030285dccc079c98635f4063ae51fa2fa0e3e
JaranOlsen <jaran.olsen@icloud.com>
2022-10-13 22:24:20 +0200

    touch controls

--- build/main.js
+++ build/main.js
@@ -1269,6 +1269,26 @@ function hoverPin() {
     }
 }
 
+function touch2Mouse(e)
+{
+  var theTouch = e.changedTouches[0];
+  var mouseEv;
+
+  switch(e.type)
+  {
+    case "touchstart": mouseEv="mousedown"; break;  
+    case "touchend":   mouseEv="mouseup"; break;
+    case "touchmove":  mouseEv="mousemove"; break;
+    default: return;
+  }
+
+  var mouseEvent = document.createEvent("MouseEvent");
+  mouseEvent.initMouseEvent(mouseEv, true, true, window, 1, theTouch.screenX, theTouch.screenY, theTouch.clientX, theTouch.clientY, false, false, false, false, 0, null);
+  theTouch.target.dispatchEvent(mouseEvent);
+
+  e.preventDefault();
+}
+
 
 document.addEventListener("keyup", onDocumentKeyUp, false);
 function onDocumentKeyUp(event) {
@@ -1525,7 +1545,9 @@ function render(time) {
 
     window.addEventListener('pointermove', onPointerMove);
     window.addEventListener('mouseup', onClick);
-    window.addEventListener('touchstart', onClick);
+    document.addEventListener("touchstart", touch2Mouse, true);
+    document.addEventListener("touchmove", touch2Mouse, true);
+    document.addEventListener("touchend", touch2Mouse, true);
     unhoverPin();
     hoverPin();
 
