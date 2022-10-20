1043732b463e36628a2ac20691fd788a66ac559c
JaranOlsen <jaran.olsen@icloud.com>
2022-10-21 00:54:05 +0200

    font fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vitePluginString from 'vite-plugin-string'
 
 export default defineConfig({
-  assetsInclude: ['models/*.glb'],
+  assetsInclude: ['models/*.glb', 'fonts/*.json'],
   base: '/Planet/',
   root: "./",
   plugins: [
