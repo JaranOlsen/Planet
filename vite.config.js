91decafc324f37ea5dc767b904c4653a76149492
JaranOlsen <jaran.olsen@icloud.com>
2022-10-21 01:08:06 +0200

    assets  fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vitePluginString from 'vite-plugin-string'
 
 export default defineConfig({
-  assetsInclude: ['models/*.glb', 'fonts/*.json'],
+  assetsInclude: ['models/*.glb'],
   base: '/Planet/',
   root: "./",
   plugins: [
