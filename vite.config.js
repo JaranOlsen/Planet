5499ff645555c0d02ca65b3950f297ad68c1019f
JaranOlsen <jaran.olsen@icloud.com>
2022-12-14 19:19:59 +0100

    low perf

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import vitePluginString from 'vite-plugin-string'
+import { resolve } from 'path'
 
 export default defineConfig({
   assetsInclude: ['models/*.glb'],
   base: '/Planet/',
   root: "./",
   plugins: [
-    vitePluginString.default(),
-  ]
+    vitePluginString(), //changed from vitePluginString.default()
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve('index.html'),
+        planet: resolve('planet.html'),
+        planet_lowperf: resolve('planet_lowperf.html'),
+        planet_ultralowperf: resolve('planet_ultralowperf.html'),
+      },
+    },
+  },
 })
