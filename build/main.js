37a369c53d52b9b88194f7bba4636363589d524c
JaranOlsen <jaran.olsen@icloud.com>
2023-02-15 20:17:30 +0100

    black screen fix

--- build/main.js
+++ build/main.js
@@ -1985,17 +1985,19 @@ function render() {
     pivot4.rotation.y += -0.0001;
     if (jaraniusInitialized == true) clouds.rotation.y += 0.00001;
 
-    if (camera.position.z > 15 && start == true) {
-        camera.position.z -= 0.0213 * Math.pow(camera.position.z - 10, 1.35) // / introTuneLength
+    if (introTuneLength) {
+        if (camera.position.z > 15 && start == true) {
+            camera.position.z -= 0.0213 * Math.pow(camera.position.z - 10, 1.35) / introTuneLength
+        }
+        if (camera.position.z < -15 && start == true) {
+            camera.position.z += 0.213 * Math.pow(Math.abs(camera.position.z) - 10, 1.35) / introTuneLength
+        }
+        if (start == true) {
+            camera.position.x += 0.4 / introTuneLength
+            camera.position.y += 0.1 / introTuneLength
+        }
     }
-    if (camera.position.z < -15 && start == true) {
-        camera.position.z += 0.213 * Math.pow(Math.abs(camera.position.z) - 10, 1.35) // / introTuneLength
-    }
-    if (start == true) {
-        camera.position.x += 0.4 // / introTuneLength
-        camera.position.y += 0.1 // / introTuneLength
-    }
-    console.log("x: ", camera.position.x, "y: ", camera.position.y, "z: ", camera.position.z)
+    //console.log("x: ", camera.position.x, "y: ", camera.position.y, "z: ", camera.position.z)
     if (camera.position.z > -15 && camera.position.z < 15) start = false
   
     controls.rotateSpeed = (camera.position.distanceTo(middleOfPlanet) - 5) / camera.position.distanceTo(middleOfPlanet);  //  /1
