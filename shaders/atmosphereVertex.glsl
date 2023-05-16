b7a6dedc18c405fd0a019fa7e35d0b376c6d73aa
JaranOlsen <jaran.olsen@icloud.com>
2023-05-16 23:48:52 +0200

    restructure

--- shaders/atmosphereVertex.glsl
+++
@@ -1,9 +0,0 @@
-varying vec3 vertexNormal;
-varying vec3 vWorldPosition;
-
-void main() {
-    vertexNormal = normalize(normalMatrix * normal);
-    vec4 worldPosition = modelMatrix * vec4(position, 1.0);
-    vWorldPosition = worldPosition.xyz;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}
