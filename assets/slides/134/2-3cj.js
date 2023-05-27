c156e8765fb24b976cb3d83d496670c57b3ba54c
JaranOlsen <jaran.olsen@icloud.com>
2023-05-27 18:06:17 +0200

    Updates

--- assets/slides/134/2-3cj.js
+++ assets/slides/134/2-3cj.js
@@ -1,9 +1,27 @@
-let currentPoint = 0;
-const bulletPoints = document.querySelectorAll(".bullet");
+(function() {
+    let currentPoint = 0;
+    const bulletPoints = document.querySelectorAll(".bullet");
+    const bulletPointsContainer = document.querySelector('.bullet-points-container');
 
-window.addEventListener('click', () => {
-    if (currentPoint < bulletPoints.length) {
-        bulletPoints[currentPoint].style.opacity = "1";
-        currentPoint++;
-    }
-});
+    const adjustFontSize = () => {
+        const numPoints = bulletPoints.length;
+        if (numPoints <= 3) {
+            bulletPointsContainer.style.fontSize = "4vw";
+        } else if (numPoints <= 5) {
+            bulletPointsContainer.style.fontSize = "3vw";
+        } else {
+            bulletPointsContainer.style.fontSize = "2vw";
+        }
+    };
+
+    window.addEventListener('click', () => {
+        if (currentPoint < bulletPoints.length) {
+            bulletPoints[currentPoint].style.opacity = "1";
+            currentPoint++;
+        }
+    });
+
+    // Call the function to set the initial font size
+    adjustFontSize();
+})();
+
