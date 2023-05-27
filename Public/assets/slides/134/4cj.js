dc4ee4410b46a3e8501beba968af1dad59ed10f9
JaranOlsen <jaran.olsen@icloud.com>
2023-05-27 18:06:03 +0200

    slides test

--- Public/assets/slides/134/4cj.js
+++ Public/assets/slides/134/4cj.js
@@ -1,25 +1,27 @@
-// slide.js
-let currentPoint = 0;
-const bulletPoints = document.querySelectorAll(".bullet");
-const bulletPointsContainer = document.querySelector('.bullet-points-container');
+(function() {
+    let currentPoint = 0;
+    const bulletPoints = document.querySelectorAll(".bullet");
+    const bulletPointsContainer = document.querySelector('.bullet-points-container');
 
-const adjustFontSize = () => {
-    const numPoints = bulletPoints.length;
-    if (numPoints <= 3) {
-        bulletPointsContainer.style.fontSize = "4vw";
-    } else if (numPoints <= 5) {
-        bulletPointsContainer.style.fontSize = "3vw";
-    } else {
-        bulletPointsContainer.style.fontSize = "2vw";
-    }
-};
+    const adjustFontSize = () => {
+        const numPoints = bulletPoints.length;
+        if (numPoints <= 3) {
+            bulletPointsContainer.style.fontSize = "4vw";
+        } else if (numPoints <= 5) {
+            bulletPointsContainer.style.fontSize = "3vw";
+        } else {
+            bulletPointsContainer.style.fontSize = "2vw";
+        }
+    };
 
-window.addEventListener('click', () => {
-    if (currentPoint < bulletPoints.length) {
-        bulletPoints[currentPoint].style.opacity = "1";
-        currentPoint++;
-    }
-});
+    window.addEventListener('click', () => {
+        if (currentPoint < bulletPoints.length) {
+            bulletPoints[currentPoint].style.opacity = "1";
+            currentPoint++;
+        }
+    });
 
-// Call the function to set the initial font size
-adjustFontSize();
+    // Call the function to set the initial font size
+    adjustFontSize();
+})();
+
