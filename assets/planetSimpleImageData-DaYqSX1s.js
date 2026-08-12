b6846f59def7b7a3abb3b9ec1dec9bc69ccba48b
JaranOlsen <jaran.olsen@icloud.com>
2026-08-12 14:11:12 +0200

    Updates

---
+++ assets/planetSimpleImageData-DaYqSX1s.js
@@ -0,0 +1,2 @@
+const s="/Planet/assets/images/features/love.webp",a="/Planet/assets/images/features/almsround.webp",e="/Planet/assets/images/features/god.webp",t=[{id:"37F54374-5342-4B56-872A-26F6D5091E23",src:s,lat:-58.8,lng:120,size:300,radius:5.06,context:"jaranius"},{id:"34A4FAAC-40A7-44FF-935B-E52240551434",src:a,lat:-58.5,lng:0,size:320,radius:5.06,context:"jaranius"},{id:"6D355AED-0F8F-4A66-B655-F61BA5183ACE",src:e,lat:-59,lng:-120,size:300,radius:5.06,context:"jaranius"}];export{t as planetImageData};
+//# sourceMappingURL=planetSimpleImageData-DaYqSX1s.js.map
